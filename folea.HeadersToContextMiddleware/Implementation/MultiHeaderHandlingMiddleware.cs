41792d011139b17f6a5ea8838f7498b12bf9ec1e
fcontreras <apps@lipigas.cl>
2021-02-23 10:26:00 -0300

    namespaces fixed

---
+++ folea.HeadersToContextMiddleware/Implementation/MultiHeaderHandlingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using folea.HeadersToContextMiddleware.Exceptions;
+using folea.HeadersToContextMiddleware.Models;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace folea.HeadersToContextMiddleware.Implementations
+{
+    public class MultiHeaderHandlingMiddleware
+    {
+        public MultiHeaderConfigurationOption[] Options { get;  }
+
+        private readonly RequestDelegate next;
+
+        
+        public MultiHeaderHandlingMiddleware(RequestDelegate next, MultiHeaderConfigurationOption[] pams)
+        {
+            Options = pams;
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            ensureHeader(context);
+            await next(context);
+        }
+
+        private void ensureHeader(HttpContext context)
+        {
+            for (int i = 0; i < Options.Length; i++)
+            {
+                var option = Options[i];
+
+                if (string.IsNullOrEmpty(option.HeaderKey)) throw new MultiHeaderHandlingMiddlewareException("HeaderKey must have a value");
+                if (option.CreateIfNotExists && (option.ValueCreationMethodIfNull is null)) throw new MultiHeaderHandlingMiddlewareException("if ValueCreationMethodIfNull is true then ValueCreationMethodIfNull must have a value");
+
+                var headerValue = context.Request.Headers[option.HeaderKey].FirstOrDefault();
+
+                if(string.IsNullOrEmpty(headerValue) && option.CreateIfNotExists)
+                {
+                    context.Items.Add(option.HeaderKey, option.ValueCreationMethodIfNull.Invoke());
+                    if (option.Propagate) context.Response.Headers.Add(option.HeaderKey, headerValue);
+                }
+                else if(!string.IsNullOrEmpty(headerValue))
+                {
+                    context.Items.Add(option.HeaderKey, headerValue);
+                    if(option.Propagate) context.Response.Headers.Add(option.HeaderKey, headerValue);
+                }
+            }            
+        }
+    }
+}
---
+++ folea.HeadersToContextMiddleware/Models/MultiHeaderConfigurationOption.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace folea.HeadersToContextMiddleware.Models
+{
+    public class MultiHeaderConfigurationOption
+    {
+        /// <summary>
+        /// The header we will be looking for, example: request-id
+        /// </summary>
+        public string HeaderKey { get; }
+        /// <summary>
+        /// Will be sended only if header exists and has a value
+        /// </summary>
+        public bool Propagate { get; }
+        /// <summary>
+        /// If the value is true then ValueCreationMethodIfNull must be 
+        /// declared
+        /// </summary>
+        public bool CreateIfNotExists { get; }
+        /// <summary>
+        /// If the value is empty or the header is not present, 
+        /// but it must be returned or created if not exists then
+        /// the function will be executed!
+        /// </summary>
+        public Func<string> ValueCreationMethodIfNull { get; }
+
+        /// <summary>
+        /// If property is true then the value will be set on Context.TraceId
+        /// </summary>
+        public bool IsTraceId { get; set; }
+
+        public MultiHeaderConfigurationOption(string headerKey,
+                                                bool isTraceId = false,
+                                                bool sendInResponse = false, 
+                                                bool createIfNotExists = false, 
+                                                Func<string> valueCreationMethodIfNull = null)
+        {
+            this.HeaderKey = headerKey;
+            this.IsTraceId = isTraceId;
+            this.Propagate = sendInResponse;
+            this.CreateIfNotExists = createIfNotExists;
+            this.ValueCreationMethodIfNull = valueCreationMethodIfNull;
+        }
+    }
+}
